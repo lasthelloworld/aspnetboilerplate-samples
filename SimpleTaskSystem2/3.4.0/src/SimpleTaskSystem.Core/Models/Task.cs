2a47d33d21bcd0b1d4f00413fb4f22838085e67e
pengyun <py@zlsoft.cn>
2018-05-30 10:22:40 +0800

    添加Task Person实体类

---
+++ SimpleTaskSystem2/3.4.0/src/SimpleTaskSystem.Core/Models/Task.cs
@@ -0,0 +1,14 @@
+﻿using Abp.Domain.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SimpleTaskSystem.Models
+{
+    public class Task: Entity<long>
+    {
+        [ForeignKey("AssignedPersonId")]
+        public virtual Person AssignedPerson { get; set; } //导航属性用于引用Person
+
+        
+
+    }
+}
